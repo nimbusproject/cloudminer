159996200536215ba3f98ee0da862c09cb47b20e
BuzzTroll <bresnaha@mcs.anl.gov>
2010-10-05 15:30:56 -1000

    adding the setup install program

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+py_version = 2.5
+Version=0.1
+
+if float("%d.%d" % sys.version_info[:2]) < py_version:
+    sys.stderr.write("Your Python version %d.%d.%d is not supported.\n" % sys.version_info[:3])
+    sys.stderr.write("cloudminer requires Python %f or newer.\n" % (py_version))
+    sys.exit(1)
+
+setup(name='cloudminer',
+      version=Version,
+      description='An persistnace API for the cloudyvents API.',
+      author='Nimbus Team',
+      author_email='nimbusteam@mcs.anl.gov',
+      url='http://www.nimbusproject.org/',
+      packages=[ 'cloudminer', 'cloudminer.tests' ],
+      long_description="""
+""",
+      license="Apache2",
+      install_requires = ["nose", "cloudyvents"],
+     )
