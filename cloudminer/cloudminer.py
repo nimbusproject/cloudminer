22b74b3811811a6ada8b900a6d0ca5bdfd9cf9fa
BuzzTroll <bresnaha@mcs.anl.gov>
2010-10-21 09:43:57 -1000

    intermediate changes

--- cloudminer/cloudminer.py
+++ cloudminer/cloudminer.py
@@ -145,6 +145,9 @@ class CloudMiner(object):
 
         return e_a
 
+    def get_events_by_name(self, cyvm, event_name):
+        self.session.query(_CYvent
+
     def get_by_iaasid(self, iaasid):
         cyvm = self.session.query(_CYVM).filter(_CYVM.iaasid == iaasid).first()
         return cyvm
